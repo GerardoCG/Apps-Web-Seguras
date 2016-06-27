556eb9c65a18117e361ccbc3e933ba0e86ae5e44
GerardoCG <gerardo305@gmail.com>
2016-06-26 19:25:48 -0700

    Practica 9
    Diplomado en seguridad informática

---
+++ dsi/dsi-web/src/main/java/uach/mx/controladores/general/InicioControlador.java
@@ -0,0 +1,19 @@
+package uach.mx.controladores.general;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class InicioControlador {
+
+	@RequestMapping(value = "/",method=RequestMethod.GET)
+	public String inicio(HttpServletRequest request, HttpServletResponse response, Principal principal){
+		return "index";
+	}
+}
---
+++ dsi/dsi-web/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<title>Web App Segura</title>
+<head>
+</head>
+<body>
+Esto es una app segura.
+</body>
+</html>
